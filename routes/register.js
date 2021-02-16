7e0a8e9e5a42f7e0d83d56b72371f657f5aede20
sohil <sohilgevariya26@gmail.com>
2021-02-16 13:59:53 +0530

    demoimage

--- routes/register.js
+++ routes/register.js
@@ -27,32 +27,40 @@ var upload = multer({
 });
 
 
-router.get('/register', async function(req, res, next) {
+router.post('/getAlldata', async function(req, res, next) {
     try{
         var records = await registerSchema.find();
-        res.json(records);
-    }catch(err){
-        res.send('Error' + err)
+        if(records.length > 0){
+            res.status(200).json({ IsSuccess: true, Data: records, Message: "Data found" });
+        }else{
+            res.status(200).json({ IsSuccess: true, Data: records, Message: "Data Not found" });
+        }
+    }catch(err0r){
+        res.status(500).json({ IsSuccess : false , Message: error.message });
     }
 });
 
 
 
-router.post("/register", async function(req,res,next){
-    const { name , email ,phone, image, password } = req.body;
+router.post("/register", upload.single('image'), async function(req,res,next){
+    const { name , email ,phone, password } = req.body;
+    var fileinfo = req.file;
     try {
-        var record = await new registerSchema({
-            name: name,
-            email: email,
-            phone: phone,
-            image: image,
-            password : password
-            
-        });
-
-        record.save();
-        if(record){
-            res.status(200).json({ IsSuccess: true , Data: record , Message: "User Register" });
+        var existrecord = await registerSchema.find( {phone: phone} );
+        if(existrecord.length == 1){
+            res.status(200).json({ IsSuccess: true, Data: [], Message: "User Already Exist" });
+        }else{
+            var record = await new registerSchema({
+                name: name,
+                email: email,
+                phone: phone,
+                password : password,
+                image: fileinfo == undefined ? " " : fileinfo.path
+            });
+            if(record !== null){
+                record.save();
+                res.status(200).json({ IsSuccess: true , Data: record , Message: "User Register" });
+            }
         }
     } catch (error) {
         res.status(500).json({ IsSuccess : false , Message: error.message });
@@ -73,32 +81,32 @@ router.post('/login', async function(req,res,next) {
     }
 });
 
-router.post('/upload', upload.single('image'), async(req, res) => {
-    const { name , email ,phone, password } = req.body;
-    var fileinfo = req.file;
-    try {
-        var record = await new registerSchema({
-            name: name,
-            email: email,
-            phone: phone,
-            password : password,
-            image: fileinfo == undefined ? " " : fileinfo.path,
-        });
-        record.save();
+// router.post('/upload', upload.single('image'), async(req, res) => {
+//     const { name , email ,phone, password } = req.body;
+//     var fileinfo = req.file;
+//     try {
+//         var record = await new registerSchema({
+//             name: name,
+//             email: email,
+//             phone: phone,
+//             password : password,
+//             image: fileinfo == undefined ? " " : fileinfo.path,
+//         });
+//         record.save();
         
-        // let a = fileinfo == undefined ? " " : fileinfo.path;
-        // console.log(a);
-        if(record){
-            res.status(200).json({ IsSuccess: true, Data: record, Message: 'File uplaoded' });
-        }else{
-            res.status(200).json({ IsSuccess: true, Data: 0, Message: 'File undefined' });
-        }
+//         // let a = fileinfo == undefined ? " " : fileinfo.path;
+//         // console.log(a);
+//         if(record){
+//             res.status(200).json({ IsSuccess: true, Data: record, Message: 'File uplaoded' });
+//         }else{
+//             res.status(200).json({ IsSuccess: true, Data: 0, Message: 'File undefined' });
+//         }
         
-    }catch(error){
-        res.status(500).json({ Message: error.message });
-    }
+//     }catch(error){
+//         res.status(500).json({ IsSuccess:false, Message: error.message });
+//     }
 
-});
+// });
 
 module.exports = router;
  
